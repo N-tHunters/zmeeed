ea6fa10d67206a23c03c00bf42e69113851a51a7
Altrul <AlexanderOnko@gmail.com>
2022-03-17 16:50:18 +0300

    Decomp

--- decompiler.py
+++ decompiler.py
@@ -1,10 +1,54 @@
 import json
-import graphviz
+import sys
+# import graphviz
+
+decompiled_blocks_global = []
+
+class Line:
+	def __init__(self, string):
+		self.string = string
+
+	def toString(self, indent=0):
+		return indent * '\t' + str(self)
+
+	def __str__(self):
+		return self.string
+
+class Condition:
+	def __init__(self, string):
+		self.string = string
+
+	def toString(self, indent=0):
+		return indent * '\t' + str(self)
+
+	def __str__(self):
+		return self.string
 
 class Block:
-	def __init__(self, lines, parent=None):
+	def __init__(self, id, parent=None):
+		self.id = id
 		self.parent = parent
-		self.lines = lines
+		self.lines = []
+
+	def addLine(self, line):
+		self.lines.append(line)
+
+	def toString(self, indent=0):
+		s = []
+		for i in self.lines:
+			if type(i) == Line:
+				s.append(i.toString(indent))
+			elif type(i) == list:
+				global decompiled_blocks_global
+				if i[1].id in decompiled_blocks_global:
+					s.append(indent * '\t' + f'if {i[0].toString()}: goto block[{i[1].id}]')
+				else:
+					decompiled_blocks_global.append(i[1].id)
+					s.append(indent * '\t' + f'')
+					s.append(i[1].toString(indent + 1))
+
+	def __str__(self):
+		return toString(self)
 
 def to_str(value, t):
 	if t == 'str':
@@ -42,7 +86,7 @@ def decompile_block(block):
 			condition = to_str(*stack.pop())
 			dest = i['argval']
 			jumps.append([int(i['jmpval']), 'false'])
-			decompiled.append(f'if {condition}: goto {dest}')
+			decompiled.append(f'if not {condition}: goto {dest}')
 		elif i['opname'] == 'BINARY_ADD':
 			op_right = to_str(*stack.pop())
 			op_left = to_str(*stack.pop())
@@ -66,7 +110,7 @@ def decompile_block(block):
 			print(i)
 	return jumps, decompiled, returns
 
-def decompile(filename):
+def decompile(filename, out_filename):
 	with open(filename) as file:
 		data = json.load(file)
 
@@ -75,7 +119,7 @@ def decompile(filename):
 
 	for i in range(len(data['blocks'])):
 		jumps, decompiled, returns = decompile_block(data['blocks'][i])
-		decompiled_blocks.append('\l'.join(decompiled))
+		decompiled_blocks.append(decompiled)
 		if not returns:
 			if len(jumps) == 0:
 				jumps.append([i + 1, ''])
@@ -87,21 +131,45 @@ def decompile(filename):
 		for j in jumps:
 			edges.append([i, j])
 
+	print(edges)
+	print(decompiled_blocks)
 
-	
-	dot = graphviz.Digraph(graph_attr={'splines': 'ortho'}, node_attr={'shape':'box'})
-	for i in range(len(decompiled_blocks)):
-		dot.node(str(i), decompiled_blocks[i])
+	data = {
+		'blocks':[],
+		'jumps':[]
+	}
+
+	for i in decompiled_blocks:
+		data['blocks'].append(i)
 
 	for i in edges:
 		if i[1][1] == 'true':
-			color = 'green'
+			color_ind = 1
 		elif i[1][1] == 'false':
-			color = 'red'
+			color_ind = -1
 		else:
-			color = 'blue'
-		dot.edge(str(i[0]), str(i[1][0]), color=color)
+			color_ind = 0
+		data['jumps'].append([i[0], i[1][0], color_ind])
 
-	dot.render('output.gv', format='png', view=True)
+	with open(out_filename, 'w') as file:
+		json.dump(data, file)
+	
+	# dot = graphviz.Digraph(graph_attr={'splines': 'ortho'}, node_attr={'shape':'box'})
+	# for i in range(len(decompiled_blocks)):
+	# 	dot.node(str(i), decompiled_blocks[i])
 
-decompile('out.json')
+	# for i in edges:
+	# 	if i[1][1] == 'true':
+	# 		color = 'green'
+	# 	elif i[1][1] == 'false':
+	# 		color = 'red'
+	# 	else:
+	# 		color = 'blue'
+	# 	dot.edge(str(i[0]), str(i[1][0]), color=color)
+
+	# dot.render('output.gv', format='png', view=True)
+
+if len(sys.argv) != 3:
+	print(f'Usage: {sys.argv[0]} <in json path> <out json path>')
+else:
+	decompile(sys.argv[1], sys.argv[2])

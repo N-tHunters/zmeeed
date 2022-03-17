c8775baa6ee0d5f2f298e17a0d75a91709bd7dfd
Magnum Malum <no@mail>
2022-03-17 14:34:28 +0300

    zmeeed basic graph GUI

--- zmeeed.py
+++ zmeeed.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QPoint
+from PyQt5.QtCore import QPoint, QEvent, Qt
+from PyQt5.QtGui import QPainter, QPen
 import sys
 import json
 import html
@@ -12,13 +13,44 @@ class MainWindow(QMainWindow):
 
     def setupUi(self):
         self.setWindowTitle("zmeeed")
+        self.setGeometry(QDesktopWidget().screenGeometry())
+
+        exitAction = QAction('&Exit', self)
+        exitAction.triggered.connect(qApp.quit)
+
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.tabs = QTabWidget(self)
+        self.tabs.move(0, 20)
+        self.tabs.resize(self.width(), self.height() - 40)
+
+        graphView = GraphView(self)
+        constsView = QLabel("Here be constants", self)
+        self.tabs.addTab(graphView, "graph")
+        self.tabs.addTab(constsView, "consts")
+
+
+class GraphView(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent = parent
+        self.setupUi()
+
+    def setupUi(self):
+        self.setGeometry(0, 40, self.parent.width(), self.parent.height() - 40)
+        self.dragStartPos = None
 
         with open("out.json") as f:
             data = json.load(f)
 
         self.blocks = []
+        self.links = []
 
         y = 30
+        x = self.width() // 2
+
 
         for block in data['blocks']:
             block_text = ''
@@ -28,17 +60,95 @@ class MainWindow(QMainWindow):
                 block_text += f'<font color="gray">{offset:0>3}:</font> <font color="blue">{opname}</font> {argval}<br/>'
 
             lbl = QLabel(block_text, self)
-            lbl.setStyleSheet('font-family: monospace')
+            lbl.setStyleSheet('font-family: monospace; border: 1px solid gray; padding: 5px')
             lbl.adjustSize()
-            lbl.move(30, y)
-
-            y += 30 + lbl.height()
+            lbl.installEventFilter(self)
 
             self.blocks.append(lbl)
+
+        self.graph = dict()
+
+        for jump in data['jumps']:
+            from_block, to_block = jump[0]
+            self.links.append((from_block, to_block, [Qt.red, Qt.blue, Qt.green][1+jump[1]], len(self.graph.get(from_block, []))))
+            if self.graph.get(from_block) is None:
+                self.graph[from_block] = []
+            self.graph[from_block].append(to_block)
+
+        queue = [(0, 0)]
+        used = [False for i in range(len(self.blocks))]
+        used[0] = True
+        self.block_strata = [0 for i in range(len(self.blocks))]
+        strata_max_height = dict()
+        while len(queue) != 0:
+            block, strata = queue[0]
+            self.block_strata[block] = strata
+            queue.pop(0)
+            if strata_max_height.get(strata) is None:
+                strata_max_height[strata] = 0
+            strata_max_height[strata] = max(strata_max_height[strata], self.blocks[block].height())
+            for child in self.graph.get(block, []):
+                if not used[child]:
+                    used[child] = True
+                    queue.append((child, strata + 1))
+
+        strata_height = [y]
+        for strata in range(1, len(strata_max_height)):
+            strata_height.append(strata_height[-1] + 30 + strata_max_height[strata-1])
+
+        strata_blocks = dict()
+        for i, block in enumerate(self.blocks):
+            strata = self.block_strata[i]
+            if strata_blocks.get(strata) is None:
+                strata_blocks[strata] = []
+            strata_blocks[strata].append((i, block))
+
+        for strata in range(len(strata_max_height)):
+            sum_width = 0
+            for i, block in strata_blocks[strata]:
+                sum_width += block.width()
+            sum_width += 30 * (len(strata_blocks[strata]) - 1)
+            sum_width //= 2
+            for i, block in strata_blocks[strata]:
+                block.move(x - sum_width, y + strata_height[self.block_strata[i]])
+                sum_width -= 30 + block.width()
+
+
+
+
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        for i, link in enumerate(self.links):
+            painter.setPen(QPen(link[2], 2, Qt.SolidLine, Qt.SquareCap, Qt.BevelJoin))
+            if self.block_strata[link[0]] < self.block_strata[link[1]]:
+                linkStartBlock = self.blocks[link[0]]
+                linkStartPos = linkStartBlock.pos() + QPoint(linkStartBlock.width() // 2, linkStartBlock.height())
+                linkStartPos += QPoint(link[3] * 4, 0)
+                linkEndBlock = self.blocks[link[1]]
+                linkEndPos = linkEndBlock.pos() + QPoint(linkEndBlock.width() // 2, 0)
+                midY = (linkEndPos.y() + linkStartPos.y()) // 2 + link[3] * 4
+                painter.drawLine(linkStartPos, QPoint(linkStartPos.x(), midY))
+                painter.drawLine(QPoint(linkStartPos.x(), midY), QPoint(linkEndPos.x(), midY))
+                painter.drawLine(QPoint(linkEndPos.x(), midY), linkEndPos)
+            else:
+                linkStartBlock = self.blocks[link[0]]
+                linkStartPos = linkStartBlock.pos() + QPoint(linkStartBlock.width() // 2, linkStartBlock.height())
+                linkStartPos += QPoint(link[3] * 4, 0)
+                linkEndBlock = self.blocks[link[1]]
+                linkEndPos = linkEndBlock.pos() + QPoint(linkEndBlock.width() // 2, 0)
+                painter.drawLine(linkStartPos, QPoint(linkStartPos.x(), linkStartPos.y() + 5 + link[3] * 4))
+                painter.drawLine(QPoint(linkStartPos.x(), linkStartPos.y() + 5 + link[3] * 4), QPoint(linkStartPos.x() + linkStartBlock.width() // 2 + 5, linkStartPos.y() + 5 + link[3] * 4))
+                painter.drawLine(QPoint(linkStartPos.x() + linkStartBlock.width() // 2 + 5, linkStartPos.y() + 5 + link[3] * 4), QPoint(linkStartPos.x() + linkStartBlock.width() // 2 + 5, linkEndPos.y() - 5))
+                painter.drawLine(QPoint(linkStartPos.x() + linkStartBlock.width() // 2 + 5, linkEndPos.y() - 5), QPoint(linkEndPos.x(), linkEndPos.y() - 5))
+                painter.drawLine(QPoint(linkEndPos.x(), linkEndPos.y() - 5), linkEndPos)
 
     def dragGraph(self, dx, dy):
         for lbl in self.blocks:
             lbl.move(lbl.pos().x() + dx, lbl.pos().y() + dy)
+        self.update()
 
     def keyPressEvent(self, event):
         if event.key() == ord('J'):
@@ -51,13 +161,26 @@ class MainWindow(QMainWindow):
             self.dragGraph(-10, 0)
 
     def mousePressEvent(self, event):
-        self.dragStartPos = event.pos()
+        self.dragStartPos = event.globalPos()
 
     def mouseMoveEvent(self, event):
-        relativeDrag = event.pos() - self.dragStartPos
-        self.dragStartPos = event.pos()
+        if self.dragStartPos is None:
+            return
+        relativeDrag = event.globalPos() - self.dragStartPos
+        self.dragStartPos = event.globalPos()
         self.dragGraph(relativeDrag.x(), relativeDrag.y())
 
+    def mouseReleaseEvent(self, event):
+        self.dragStartPos = None
+
+    def eventFilter(self, obj, event):
+        if event.type() == QEvent.MouseButtonPress:
+            self.mousePressEvent(event)
+        if event.type() == QEvent.MouseMove:
+            self.mouseMoveEvent(event)
+        if event.type() == QEvent.MouseButtonRelease:
+            self.mouseReleaseEvent(event)
+        return super(QWidget, self).eventFilter(obj, event)
 
 
 
